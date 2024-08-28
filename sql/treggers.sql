1672b959488bccefc8d54d8177e3f5050c83d77e
Josequake <jose_quake@hotmail.com>
2024-08-28 00:24:24 -0600

    agregacones de trigger y actualizacion en stored

---
+++ sql/treggers.sql
@@ -0,0 +1,43 @@
+
+
+DELIMITER //
+
+-----------------------------------------------Actualiza el estado de la habitacion a "no disponible" por si se elimino una reservacion-------------------------------------------
+
+CREATE TRIGGER trg_update_disponibilidad_reserva
+AFTER INSERT ON reservas
+FOR EACH ROW
+BEGIN
+    UPDATE habitacion
+    SET reservada = 'No disponible'
+    WHERE ID_Habitacion = NEW.ID_Habitacion;
+END //
+
+-----------------------------------------------Actualiza el estado de la habitacion a "disponible" por si se agrego una reservacion-------------------------------------------
+CREATE TRIGGER disponibilidad
+AFTER DELETE ON reservas
+FOR EACH ROW
+BEGIN
+    UPDATE habitacion
+    SET reservada = 'Disponible'
+    WHERE ID_Habitacion = OLD.ID_Habitacion;
+END //
+
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS trg_update_disponibilidad_reserva
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
